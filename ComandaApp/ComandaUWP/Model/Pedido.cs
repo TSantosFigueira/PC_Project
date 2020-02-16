837401612a75b708e720cbc707d8ac34e1347700
Thiago Santos Figueira <tsantosfigueira@outlook.com>
2020-02-16 15:42:16 -0400

    Criada a visualizacao de vendas

---
+++ ComandaApp/ComandaUWP/Model/Pedido.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComandaUWP.Model
+{
+
+    public enum FormaPgto
+    {
+        debito,
+        credito,
+        dinheiro
+    }
+
+    public class Pedido
+    {
+        public int NumeroPedido { get; set; }
+        public double PrecoTotal { get; set; } 
+        public DateTime Hora { get; set; }
+        public FormaPgto FormaPgto { get; set; }
+        public string NomeCliente { get; set; }
+
+        public Pedido(int numPedido, double precoTotal, FormaPgto pgto, string cliente)
+        {
+            NumeroPedido = numPedido;
+            PrecoTotal = precoTotal;
+            Hora = DateTime.Now;
+            FormaPgto = pgto;
+            NomeCliente = cliente;
+        }
+    }
+}
---
+++ ComandaApp/ComandaUWP/ViewModel/PedidoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ComandaUWP.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComandaUWP.ViewModel
+{
+    public class PedidoViewModel
+    {
+        public ObservableCollection<Pedido> Pedidos;
+
+        public PedidoViewModel()
+        {
+            Pedidos = new ObservableCollection<Pedido>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Pedido pedido = new Pedido(i, 40, FormaPgto.debito, "Jorge");
+                Pedidos.Add(pedido);
+            }
+        }
+    }
+}
